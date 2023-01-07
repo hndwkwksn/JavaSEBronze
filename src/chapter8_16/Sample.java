2aaecdf7f0c56578155381bf46d1d30fbce7e237
hndwkwksn <hondawakuwakusan0630@gmail.com>
2023-01-07 20:08:57 +0900

    commit

---
+++ src/chapter8_16/Sample.java
@@ -0,0 +1,9 @@
+package chapter8_16;
+
+public interface Sample {
+	public void setVal1(String val);
+//	private void setVal2(String val);
+//	String val;
+	void setVal4(String val);
+//	public static void setVal5(String val);
+}
